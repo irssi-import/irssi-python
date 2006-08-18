501f3900176b1299dad452d95bab9d17b4e50fba
Christopher Davis <loafier@gmail.com>
2006-08-18 06:57:46 +0000

    added more scripts

--- src/irssi.py
+++ src/irssi.py
@@ -29,3 +29,30 @@ def io_add_watch(*args, **kwargs):
     """ see Script.io_add_watch """
     get_script().io_add_watch(*args, **kwargs)
 
+def statusbar_item_register(*args, **kwargs):
+    """ see Script.statusbar_item_register """
+    get_script().statusbar_item_register(*args, **kwargs)
+
+def settings_add_str(*args, **kwargs):
+    """ see Script.settings_add_str """
+    get_script().settings_add_str(*args, **kwargs)
+
+def settings_add_int(*args, **kwargs):
+    """ see Script.settings_add_int """
+    get_script().settings_add_int(*args, **kwargs)
+
+def settings_add_bool(*args, **kwargs):
+    """ see Script.settings_add_bool """
+    get_script().settings_add_bool(*args, **kwargs)
+
+def settings_add_time(*args, **kwargs):
+    """ see Script.settings_add_time """
+    get_script().settings_add_time(*args, **kwargs)
+
+def settings_add_level(*args, **kwargs):
+    """ see Script.settings_add_level """
+    get_script().settings_add_level(*args, **kwargs)
+
+def settings_add_size(*args, **kwargs):
+    """ see Script.settings_add_size """
+    get_script().settings_add_size(*args, **kwargs)
