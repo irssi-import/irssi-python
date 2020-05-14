edb2ca7044815b57e6eec9a08264ad705fdd0675
Ailin Nemui <ailin@z30a.localdomain>
2020-05-14 02:10:08 +0200

    update hello script

--- scripts/hello.py
+++ scripts/hello.py
@@ -8,13 +8,13 @@ import irssi
 #         or None if the window is empty
 def cmd_hello(data, server, witem):
     if not server or not server.connected:
-        irssi.prnt("Not connected to server")
+        irssi.prnt(b"Not connected to server")
 
     if data:
-        server.command("MSG %s Hello!" % data)
+        server.command(b"MSG %s Hello!" % data)
     elif isinstance(witem, irssi.Channel) or isinstance(witem, irssi.Query):
-        witem.command("MSG %s Hello!" % witem.name)
+        witem.command(b"MSG %s Hello!" % witem.name)
     else:
-        irssi.prnt("Nick not given, and no active channel/query in window")
+        irssi.prnt(b"Nick not given, and no active channel/query in window")
 
-irssi.command_bind('hello', cmd_hello)
+irssi.command_bind(b'hello', cmd_hello)
